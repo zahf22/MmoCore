aa7e336acbd4af498dc8b56d5f8c6f092515f26b
zahf22 <rasecsantos22@gmail.com>
2018-11-12 22:33:41 +0800

    Update and rename Test.php to FriendCommands.php

---
+++ src/MmoRpg/SubCommands/FriendCommands.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace MmoCore;
+
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\Player;
+use pocketmine\Server;
+use pocketmine\utils\TextFormat;
+
+class SubCommands {
+}
